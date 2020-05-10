bd37b51fea585b60f617274634da1df4f5fa9d20
Yuri018 <plktmn@gmail.com>
2020-05-10 14:02:30 +0500

    added solution NumberOfYears

--- 04_NumbersStringsAndDates/StringExperiments/src/NumberOfYears.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/NumberOfYears.java
@@ -6,14 +6,21 @@ import java.util.GregorianCalendar;
 
 public class NumberOfYears {
     public static void main(String[] args){
-        Calendar calendar = new GregorianCalendar(1968, Calendar.OCTOBER, 23);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE - dd.MM.yyyy - MMM");
 
-        String oneDay = dateFormat.format(calendar.getTime());
-        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - день недели - EEEE");
+        Calendar birthday = new GregorianCalendar(1968, Calendar.OCTOBER, 23);
+        Calendar currentDay = new GregorianCalendar();
 
-        System.out.println(oneDay);
-        System.out.println(dateFormat.format(date));
+        int a = birthday.get(Calendar.YEAR);
+        int b = currentDay.get(Calendar.YEAR);
+        int c = b - a;
 
+        System.out.println("День рождения: " + dateFormat.format(birthday.getTime()));
+        System.out.println("Сегодня: " + dateFormat.format(currentDay.getTime()));
+
+        for (int i = 0; i < (c); i++){
+            birthday.set(Calendar.YEAR, (a + i));
+            System.out.println("Возраст: " + i + ". Дата: " + dateFormat.format(birthday.getTime()));
+        }
     }
 }
