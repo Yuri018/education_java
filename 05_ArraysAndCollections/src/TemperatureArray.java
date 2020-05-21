9f2bc0e5f17a62e7325d7185580b831a7efff5e4
Yuri018 <plktmn@gmail.com>
2020-05-21 12:30:43 +0500

    arbeiten an TemperatureArray

--- 05_ArraysAndCollections/src/TemperatureArray.java
+++ 05_ArraysAndCollections/src/TemperatureArray.java
@@ -3,16 +3,22 @@ public class TemperatureArray {
         double[] temperatureArray = new double[30];
         double sum = 0;
         double average = 0;
+        double temp = 0;
 
         for (int i = 0; i < temperatureArray.length; i++) {
-            double temp = 32 + (Math.random() * 8);
-            temperatureArray[i] = temp;
+            temperatureArray[i] = 32 + (Math.random() * 8);
+//            temperatureArray[i] = temp;
             sum += temperatureArray[i];
             average = sum / temperatureArray.length;
+            if (temperatureArray[i] >= 36.2 && temperatureArray[i] <= 36.9){
+                temp = temperatureArray[i];
+//                System.out.printf("Здоровые пациенты %d: %.1f\u00B0C;", (i + 1),temperatureArray[i]);
+            }
         }
         for (int i = 0; i < temperatureArray.length; i++) {
             System.out.printf("Пациент %d: %.1f\u00B0C; %n",(i + 1) , temperatureArray[i]);
         }
-        System.out.printf("Средняя температура: %.1f\u00B0C", average);
+        System.out.printf("Средняя температура: %.1f\u00B0C %n", average);
+        System.out.println(temp);
     }
 }
