389e4b713889cd7e1072dadd43b04242433d114d
Yuri018 <plktmn@gmail.com>
2020-05-26 16:56:52 +0500

    kleine Arbeitet

---
+++ 05_ArraysAndCollections/src/ArrayX.java
@@ -0,0 +1,22 @@
+public class ArrayX {
+    public static void main(String[] args) {
+        int n = 7;
+        String[][] letterX = new String[n][n];
+        for (int i = 0; i < letterX.length; i++) {
+            for (int j = 0; j < letterX.length; j++) {
+                letterX[i][i] = "x";
+                letterX[i][(n - 1) - j] = " ";
+                if (i == j){
+                    letterX[i][j] = "x";
+                    letterX[i][n - 1 - j] = "x";
+                }
+            }
+        }
+        for (int i = 0; i < letterX.length; i++) {
+            for (int j = 0; j < letterX.length; j++) {
+                System.out.print(letterX[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- 05_ArraysAndCollections/src/Arrays.java
+++ 05_ArraysAndCollections/src/Arrays.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Arrays {
     public static void main(String[] args) {
+
+        int x = 1;
+
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
@@ -10,14 +13,14 @@ public class Arrays {
         //инициялизация (заполнение) массива
         for (int i = 0; i < array2.length; i++) {
             for (int j = 0; j < array2.length; j++) {
-                array2[i][j] = i * j + 1;
+                array2[i][j] = x;
             }
         }
         for (int i = 0; i < a; i++) {
             for (int j = 0; j < b; j++) {
                 System.out.print(" " + array2 [i][j]);
             }
-            System.out.println("\n");
+            System.out.println();
         }
     }
 }
