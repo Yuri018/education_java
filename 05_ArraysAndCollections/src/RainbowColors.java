8644f60fa2168f657472c21f952f59fb35b7f7f8
Yuri018 <plktmn@gmail.com>
2020-06-11 10:43:03 +0500

    revision RainbowColors

--- 05_ArraysAndCollections/src/RainbowColors.java
+++ 05_ArraysAndCollections/src/RainbowColors.java
@@ -7,14 +7,28 @@ public class RainbowColors {
         }
         System.out.println("--------------------------");
 
-        //второй вариант перебора массива, если не нужен индекс элемента
+        int n = rainbowColors.length;
+        String temp;//временная переменная для хранения значений
+//для замены элементов массива местами необходимо пройти только половину массива n/2
+        for (int i = 0; i < n/2; i++) {
+            temp = rainbowColors[n - i - 1];
+            rainbowColors[n - i - 1] = rainbowColors[i];
+            rainbowColors[i] = temp;
+        }
+//        for (int i = 0; i < rainbowColors.length; i++) {
+//            System.out.println(rainbowColors[i]);
+//        }
         for (String x: rainbowColors) {
             System.out.println(x);
         }
-        System.out.println("--------------------------");
-        // цикл для перебора элементов массива в обратном порядке
-        for (int i = rainbowColors.length - 1; i >= 0 ; i--) {
-            System.out.println(rainbowColors[i]);
-        }
+//        //второй вариант перебора массива, если не нужен индекс элемента
+//        for (String x: rainbowColors) {
+//            System.out.println(x);
+//        }
+//        System.out.println("--------------------------");
+//        // цикл для перебора элементов массива в обратном порядке
+//        for (int i = rainbowColors.length - 1; i >= 0 ; i--) {
+//            System.out.println(rainbowColors[i]);
+//        }
     }
 }
