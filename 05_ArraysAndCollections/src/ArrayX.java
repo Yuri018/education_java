5290b2a136da4643a526e49e428cf30cdb15b434
Yuri018 <plktmn@gmail.com>
2020-06-12 14:39:19 +0500

    if block change in ArrayX

--- 05_ArraysAndCollections/src/ArrayX.java
+++ 05_ArraysAndCollections/src/ArrayX.java
@@ -1,11 +1,11 @@
 public class ArrayX {
     public static void main(String[] args) {
-        int n = 7;
+        int n = 6;
         String[][] letterX = new String[n][n];
         for (int i = 0; i < letterX.length; i++) {
             for (int j = 0; j < letterX[i].length; j++) {
-                letterX[j][(n - 1) - j] = "x";
-                if (i == j){
+//                letterX[j][(n - 1) - j] = "x";
+                if (i == j || i + j == n - 1){
                     letterX[i][j] = "x";
                 }
                 else letterX[i][j] = " ";
