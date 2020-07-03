4445aff4f3a5954b6d9c3788cbff50a316c44cff
Yuri018 <plktmn@gmail.com>
2020-07-03 13:51:21 +0500

    arbeit mit TodoList

--- 05_ArraysAndCollections/src/Test.java
+++ 05_ArraysAndCollections/src/Test.java
@@ -1,9 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args){
-        String a = "10";
-        int b = Integer.parseInt(a) + 2;
-        System.out.println(b);
-        String c = Integer.toString(b);
-        System.out.println(c);
+        System.out.println("Список команд: LIST открывает список, ADD добавляет что-то в список, DELETE удаляет что-то из списка");
+        System.out.println("Вызовите команду : ");
+        ArrayList<String> todoList = new ArrayList<>() {{
+            add(0, "Почистить зубы");
+            add(1, "Сделать зарядку");
+            add(2, "Выпить стакан воды");
+            add(3, "Приготовить завтрак");
+            add(4, "Отправиться на работу");
+
+        }};
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            switch (sc.nextLine()) {
+                case "LIST":
+                    System.out.println("Ваш список дел: " + todoList);
+                    continue;
+
+                case "ADD":
+                    System.out.println("Добавить дело в список: ");
+                    todoList.add(sc.nextLine());
+                    System.out.println("Дело добавленно!" + " номер в списке: " + todoList.size());
+                    continue;
+
+                case "DELETE":
+                    System.out.println("Удалить дело из списка: ");
+                    todoList.remove(sc.nextInt() - 1);
+                    System.out.println("Дело удалено! " + todoList.size());
+                    continue;
+
+                default:
+                    System.out.println("Давайте выберем действие!");
+                    break;
+            }
+            sc.close();
+        }
+
     }
 }
