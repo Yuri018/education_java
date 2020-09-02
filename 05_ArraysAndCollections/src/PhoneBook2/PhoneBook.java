ebc505c979ef84a2e62dd7bbfbe5df303eebb2ab
yuri018 <plktmn@gmail.com>
2020-09-02 14:53:54 +0500

    das ist finale Losunge fur PhoneBook

--- 05_ArraysAndCollections/src/PhoneBook2/PhoneBook.java
+++ 05_ArraysAndCollections/src/PhoneBook2/PhoneBook.java
@@ -1,62 +1,86 @@
 package PhoneBook2;
 
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
 
 public class PhoneBook {
 
     public static void main(String[] args) {
         TreeMap<String, String> phoneBook = new TreeMap<>();
 
-        phoneBook.put("+71110123456", "Yuri");
-        phoneBook.put("+72220123456", "Michel");
+        phoneBook.put("+7 (111) 012-34-56", "Yuri");
+        phoneBook.put("+7 (222) 012-34-56", "Michel");
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Для просмотра списка введите команду \"LIST\"\n" +
+                "для добавления нового контакта введите имя или номер\n" +
+                "для выхода введите \"EXIT\"");
         for (; ; ) {
+            System.out.println("Ввод:");
             String userInput = scanner.nextLine();
+
             if (userInput.matches("list")) {
                 printPhoneBook(phoneBook);
-            } else if (userInput.matches("[ \\t]*(\\+?\\d{11})")) {
-                if (phoneBook.containsKey(userInput)) {
-                    System.out.println("ключ - " + userInput + " значение - " + phoneBook.get(userInput));
+            } else if (userInput.matches("exit")) {
+                break;
+            } else if (userInput.matches("(\\+[0-9]\\d{10}|\\d{11})")) {
+                String i = formatNumber(userInput);
+                if (phoneBook.containsKey(i)) {
+                    System.out.println("Имя: " + phoneBook.get(i) + " номер: " + i);
                 } else {
                     System.out.println("введите имя для нового контакта");
                     String name = scan();
-                    if (name.matches("[a-zA-Z]*")) {
-                        phoneBook.put(userInput, name);
-                        System.out.println("ключ - " + userInput + " значение - " + name);
+
+                    if (name.matches("([a-zA-Z\\s]+)")) {
+                        String upperCase = firstUpperCase(name);
+                        phoneBook.put(i, upperCase);
+                        System.out.println("Новый контакт:\n" + "Имя: " + upperCase + " номер: " + i);
                     }
                 }
-            } else if (userInput.matches("[a-zA-Z]*")) {
-                if (phoneBook.containsValue(userInput)) {
+            } else if (userInput.matches("([a-zA-Z\\s]+)")) {
+                String upperCase = firstUpperCase(userInput);
+                if (phoneBook.containsValue(upperCase)) {
                     for (Map.Entry<String, String> v : phoneBook.entrySet()) {
-                        if (v.getValue().equals(userInput)) {
-                            System.out.println("ключ - " + v.getKey() + " значение -  " + v.getValue());
+                        if (v.getValue().equals(upperCase)) {
+                            System.out.println("Имя: " + v.getValue() + " номер: " + v.getKey());
                         }
                     }
                 } else {
                     System.out.println("Введите номер нового контакта");
                     String key = scan();
-                    if (key.matches("[ \\t]*(\\+?\\d{11})")) {
-                        phoneBook.put(key, userInput);
+                    if (key.matches("(\\+?\\d{11})")) {
+                        String i = formatNumber(key);
+                        phoneBook.put(i, upperCase);
+                        System.out.println("Новый контакт:\n" + "Имя: " + upperCase + " номер: " + i);
                     }
                 }
             }
         }
     }
 
-    //метод вывода на печать
+    //метод печати с сортировкой значений в алфавитном порядке
     static void printPhoneBook(Map<String, String> map) {
-        for (String book : map.keySet()) {
-            System.out.println(book + " " + map.get(book));
-        }
+        map.entrySet().stream().sorted(Map.Entry.comparingByValue())
+                .forEach(x -> System.out.println("Имя: " + x.getValue() +  " номер: " + x.getKey()));
+        //стандартный метод печати с сортировкой по ключам
+//        for (String book : map.keySet()) {
+//            System.out.println(book + " " + map.get(book));
+//        }
     }
 
     public static String scan() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
+
+    //Делаем первую букву заглавной (необходимо для правильной сортировки по значению)
+    public static String firstUpperCase(String big) {
+        return big.substring(0, 1).toUpperCase() + big.substring(1);
+    }
+
+    // Приводим номер к единому формату
+    static String formatNumber(String number) {
+        return number.replaceAll("(\\+\\d|\\d)?(\\d{3})(\\d{3})(\\d{2})(\\d{2})",
+                "+7 ($2) $3-$4-$5");
+    }
 }
+
