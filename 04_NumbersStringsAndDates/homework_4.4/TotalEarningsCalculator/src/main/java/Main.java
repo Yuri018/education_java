7cbf0e53a82c3fcad8a452b119f5f02963b8acbc
yuri018 <plktmn@gmail.com>
2021-02-20 13:22:29 +0500

    Lösung der TotalEarningsCalculator-Aufgabe mit Tests

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,18 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+
+        String profit1 = text.substring(text.indexOf("ал") + 3, text.indexOf(" руб"));
+        String profit2 = text.substring(text.indexOf("-") + 2, text.lastIndexOf(",") - 6);
+        String profit3 = text.substring(text.lastIndexOf("-") + 2, text.lastIndexOf(" руб"));
+
+        int sum = Integer.parseInt(profit1) + Integer.parseInt(profit2)
+                + Integer.parseInt(profit3);
+
+        System.out.println(sum);
+
+    }
 
 }
