61b90c614172ac329f30e8d1277eeec99d4a109b
yuri018 <plktmn@gmail.com>
2021-02-20 12:18:32 +0500

    Lösung von TrucksContainersAndBoxes-Aufgabe mit Tests

--- 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -1,24 +1,34 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int container = 27;
+    public static int truck = 12;
 
     public static void main(String[] args) {
+
+//        System.out.println("Введите количество ящиков");
         Scanner scanner = new Scanner(System.in);
-        String boxes = scanner.nextLine();
+        int box = scanner.nextInt();
+        //максимальное количество ящиков в контейнере задаем по условию:
+        int needCont = (box / container) <= container ? container : container++;
+        //максимальное количество контейнеров в грузовике задаем по условию:
+        int needTruck = (needCont / truck) <= truck ? truck : truck++;
+        int numCont = 0;
+        int numTruck = 0;
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
-
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
+        for (int i = 1; i <= box; i++) {
+            if ((i % (needTruck * needCont)) == 1) {
+                numTruck++;
+                System.out.println("Грузовик: " + numTruck);
+            }
+            if (i % needCont == 1) {
+                numCont++;
+                System.out.println("\t" + "Контейнер: " + numCont);
+            }
+            System.out.println("\t" + "\t" + "Ящик: " + i);
+        }
+        System.out.println("Необходимо:");
+        System.out.println("грузовиков - " + numTruck + " шт.");
+        System.out.println("контейнеров - " + numCont + " шт.");
     }
-
 }
