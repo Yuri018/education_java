7d29a5fdaced5980c8a173a736fabd32746538e7
yuri018 <plktmn@gmail.com>
2021-02-26 12:14:20 +0500

    Lösung der TotalEarningsCalculatorReqExp-Aufgabe mit Tests

--- 04_NumbersStringsAndDates/StringExperiments/src/NumberOfYears.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/NumberOfYears.java
@@ -1,7 +1,6 @@
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class NumberOfYears {
--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        String text = "Вася 580заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей 437 ";
+        calculateSalarySum(text);
 
-  }
+    }
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
+    public static int calculateSalarySum(String text) {
+
+        List<Integer> listCount = new ArrayList<>();
+        int count;
+        int result = 0;
+
+        Pattern pattern = Pattern.compile("(\\d+)");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            count = Integer.parseInt(matcher.group());
+            listCount.add(count);
+            System.out.println(listCount.size());
+        }
+        for (int i : listCount) {
+            result += i;
+        }
+        System.out.println(result);
+        return result;
+    }
 
 }
