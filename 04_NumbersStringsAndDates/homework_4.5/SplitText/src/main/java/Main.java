4f810535c65c46e27ff1cc3d5565443be850bf7e
yuri018 <plktmn@gmail.com>
2021-03-02 13:46:57 +0500

    Lösung der SplitText-Aufgabe mit Tests

--- 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
@@ -1,12 +1,22 @@
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
+    }
 
-  public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
-  }
+    public static String splitTextIntoWords(String text) {
+
+        String[] words = text.replaceAll("\\d", "").split("\\s*(\\s|,|;|-|!|\\.)\\s*");
+        //методом replaceAll заменяем ненужные строки, методом split убираем ненужные символы
+        StringBuilder word = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            if (i != words.length - 1) {
+                word.append(words[i]).append("\r\n");
+            } else
+                word.append(words[i]);
+        }
+        return word.toString();
+    }
 
 }
