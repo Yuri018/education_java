255e349577108aec964f6101b0757dcd4ad86e08
yuri018 <plktmn@gmail.com>
2021-03-03 13:57:19 +0500

    Lösung der FullNameFormatterRegExp-Aufgabe mit Tests

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,18 +1,30 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            Pattern pattern = Pattern.compile("[А-ЯЁа-яё\\-]\\s+");
+            Matcher matcher = pattern.matcher(input);
+            String[] fullName = input.split("\\s");
+//            System.out.println(fullName.length);
+            if (matcher.find()) {
+                if (fullName[0].equals(fullName[1]) || fullName[0].equals(fullName[2])) {
+                    System.out.println("Введенная строка не является ФИО");
+                } else {
+                    System.out.printf("Фамилия: %s\nИмя: %s\nОтчество: %s", fullName[0], fullName[1], fullName[2]);
+                }
+            } else {
+                System.out.println("Введенная строка не является ФИО");
+            }
+        }
     }
-  }
-
 }
