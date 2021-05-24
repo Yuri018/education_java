44f95819c3599b3cdaba8de78afdb6835d7702a3
yuri018 <plktmn@gmail.com>
2021-05-24 14:46:10 +0500

    Aufgabe 04_06_NumberStringAndDate homework_6.3 TrucksContainersAndBoxes abgeschlossen

--- 04_06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -2,9 +2,34 @@ import java.util.Scanner;
 
 public class Main {
 
+     static int  maxBoxInCont = 27;
+    static int maxContInTrack = 12;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
+        int box = Integer.parseInt(boxes);
+
+        int countCont = (box / maxBoxInCont) <= maxBoxInCont? maxBoxInCont: maxBoxInCont++;
+        int countTrack = (countCont / maxContInTrack) <= maxContInTrack? maxContInTrack:
+                maxContInTrack++;
+
+        int numCont = 0;
+        int numTrack = 0;
+
+        for (int i = 1; i <= box; i++){
+            if ((i % (countCont * countTrack)) == 1){
+                numTrack++;
+                System.out.println("Грузовик: " + numTrack);
+            }
+            if (i % countCont == 1){
+                numCont++;
+                System.out.println("\t" + "Контейнер: " + numCont);
+            }
+            System.out.println("\t" + "\t" + "Ящик: " + i);
+        }
+        System.out.println("Необходимо:\n" + "грузовиков - " + numTrack + " шт.\n"
+        + "контейнеров - " + numCont + " шт.");
 
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
