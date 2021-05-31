d66e61934b39b61bbef90a7e5d217096bd44f71a
yuri018 <plktmn@gmail.com>
2021-05-31 10:53:27 +0500

    Aufgabe 04_06_NumberStringAndDate homework_6.4.2 TotalEarningsCalculator abgeschlossen

--- 04_06_NumbersStringsAndDates/homework_6.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_06_NumbersStringsAndDates/homework_6.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
     //TODO: напишите ваш код, результат вывести в консоль
+    int vasyaProfit = Integer.parseInt
+            (text.substring(text.indexOf("л ") + 2, text.indexOf(" р")));
+    int petyaProfit = Integer.parseInt
+            (text.substring(text.indexOf("- ") + 2, text.indexOf(" рубля")));
+    int machaProfit = Integer.parseInt
+            (text.substring(text.indexOf("а - ") + 4, text.lastIndexOf(" рублей")));
+    int sum = vasyaProfit + petyaProfit + machaProfit;
+    System.out.println(sum);
   }
-
 }
