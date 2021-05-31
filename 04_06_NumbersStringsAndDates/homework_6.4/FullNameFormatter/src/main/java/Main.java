80705ac2585db341862872ba3f23672882f5cea8
yuri018 <plktmn@gmail.com>
2021-05-31 16:17:50 +0500

    Aufgabe 04_06_NumberStringAndDate homework_6.4.3 FullNameFormatter abgeschlossen

--- 04_06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
+++ 04_06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
@@ -1,18 +1,41 @@
 import java.util.Scanner;
+import static java.lang.Character.isDigit;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            int spaceCount = 0;
+            boolean hasDigit = false;
+            input = input.trim();
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == ' '){
+                    spaceCount++;
+                }
+                if (isDigit(input.charAt(i))) {
+                    hasDigit = true;
+                    break;
+                }
+            }
+            if (spaceCount != 2 || hasDigit) {
+                System.out.println("Введенная строка не является ФИО");
+                return;
+            } else {
+                String surname = input.substring(0, input.indexOf(" "));
+                String withoutSurname = input.substring(input.indexOf(" ") + 1);
+                String name = withoutSurname.substring(0, withoutSurname.indexOf(" "));
+                String fatherName = withoutSurname.substring(withoutSurname.indexOf(" ") + 1);
+                System.out.println("Фамилия: " + surname);
+                System.out.println("Имя: " + name);
+                System.out.println("Отчество: " + fatherName);
+            }
+        }
     }
-  }
-
 }
