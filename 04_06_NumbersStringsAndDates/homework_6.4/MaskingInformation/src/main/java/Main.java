c5a9b50ceb8a015c50305312c32f3f368f4f5297
yuri018 <plktmn@gmail.com>
2021-06-04 11:56:01 +0500

    Aufgabe 04_06_NumberStringAndDate homework_6.4.* MaskingInformation abgeschlossen

--- 04_06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
+++ 04_06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
@@ -1,12 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-
+//        searchAndReplaceDiamonds("Номер кредитной карты <4008> 1234 <5678> 8912", "***");
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
-    }
 
+        StringBuilder textPlaceholder = new StringBuilder(text);
+
+        while (textPlaceholder.indexOf("<") != -1 && textPlaceholder.indexOf(">") != -1) {
+            int textInDiamondOpen = textPlaceholder.indexOf("<");
+            int textInDiamondClose = textPlaceholder.indexOf(">");
+
+            textPlaceholder.replace(textInDiamondOpen, textInDiamondClose + 1, placeholder);
+        }
+        System.out.println(textPlaceholder);
+        return textPlaceholder.toString();
+    }
 }
