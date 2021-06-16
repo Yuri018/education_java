1ef9c525c9d4cd750366edab5af6670c76f0b76e
yuri018 <plktmn@gmail.com>
2021-06-16 13:37:52 +0500

    Methoden zum Arbeiten mit der Stream-API

--- 09_AdvancedOOPFeatures/homework_9.1/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.1/src/main/java/Main.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -9,7 +11,6 @@ public class Main {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         sortBySalaryAndAlphabet(staff);
     }
-
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
         // eine variante
 //        Collections.sort(staff, new Comparator<Employee>() {
@@ -19,7 +20,8 @@ public class Main {
 //            }
 //        });
         //zweite variante mit lambda
-        //Collections.sort(staff, (o1, o2) -> o1.getSalary().compareTo(o2.getSalary()));
+        //Collections.sort(staff, (o1, o2) -> o1.getSalary()
+        // .compareTo(o2.getSalary()));
 
         //dritte Kurzfassung
         // staff.sort(Comparator.comparing(Employee::getSalary));
@@ -34,10 +36,40 @@ public class Main {
 //        });
 
         //Lösung mit Comparator.thenComparing
-        staff.sort(Comparator.comparing(Employee::getSalary).thenComparing(Employee::getName));
+        staff.sort(Comparator.comparingInt(Employee::getSalary)
+                .thenComparing(Employee::getWorkStart));
 
-        for (Employee employee : staff) {
-            System.out.println(employee);
-        }
+//        for (Employee employee : staff) {
+//            System.out.println(employee);
+//        }
+        staff.forEach(System.out::println);
+
+//        staff.forEach(employee -> {
+//            int salary = employee.getSalary();
+//            employee.setSalary(salary + 10000);
+//        });
+        staff.forEach(employee -> employee.setSalary(employee.getSalary() + 10000));
+
+        System.out.println("------------------");
+        staff.forEach(System.out::println);
+
+        System.out.println("------------------");
+
+        // wir machen variante mit Stream
+
+        Stream<Employee> stream = staff.stream();
+        stream.filter(employee -> employee.getSalary() >= 100000).forEach(System.out::println);
+
+        //kurze variante
+        System.out.println("------------------");
+        staff.stream().filter(e -> e.getSalary() >= 110000).forEach(System.out::println);
+
+        //Stream bekommen
+
+        Stream<Integer> integerStream = Stream.of(1,2,3,4,5,6);
+        integerStream.filter(number -> number % 2 == 0).forEach(System.out::println);
+
+        Integer[] numbers = {1, 2, 3, 4, 5};
+        Arrays.stream(numbers).forEach(System.out::println);
     }
 }
