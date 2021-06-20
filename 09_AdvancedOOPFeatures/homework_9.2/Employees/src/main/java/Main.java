db34268d94171dd8bf706833f761ef0e7a3c1d1e
yuri018 <plktmn@gmail.com>
2021-06-20 11:50:54 +0500

    Aufgabe 09_AdvancedOOPFeatures/homework_9.2/Employees abgeschlossen

--- 09_AdvancedOOPFeatures/homework_9.2/Employees/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.2/Employees/src/main/java/Main.java
@@ -1,8 +1,10 @@
+import java.time.ZoneId;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 
-    private static final String STAFF_TXT = "data/staff.txt";
+    private static final String STAFF_TXT = "Employees/data/staff.txt";
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
@@ -13,6 +15,11 @@ public class Main {
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
         //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
         // кто пришёл в году, указанном в переменной year
-        return null;
+        return staff.stream()
+                .filter(employee -> employee.getWorkStart()
+                        .toInstant().atZone(ZoneId.systemDefault())
+                        .toLocalDate().getYear() == year)
+                .max(Comparator.comparingInt(Employee::getSalary))
+                .orElse(null);
     }
 }
