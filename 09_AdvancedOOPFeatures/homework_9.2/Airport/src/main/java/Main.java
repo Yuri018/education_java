e97a3e8ffd354338508a91c2f82b359ca4785e54
yuri018 <plktmn@gmail.com>
2021-06-24 14:27:39 +0500

    Aufgabe 09_AdvancedOOPFeatures/homework_9.2/Airport abgeschlossen

--- 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
@@ -1,18 +1,31 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
 
-import java.util.Collections;
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        System.out.println(findPlanesLeavingInTheNextTwoHours(Airport.getInstance()));
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
+        long currentTime = new Date().getTime();
+        long departureNextTwoHours = 60 * 60 * 2 * 1000;
 
+        return airport.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream())
+//                .map(Terminal::getFlights)
+//                .flatMap(Collection::stream)
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE))
+                .filter(time -> time.getDate().getTime() >= currentTime &&
+                        time.getDate().getTime() <= currentTime + departureNextTwoHours)
+                .collect(Collectors.toList());
+
+    }
 }
