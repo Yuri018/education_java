bac30b8673660a3a32bb64ca2fc0cfc4221938f6
yuri018 <plktmn@gmail.com>
2021-09-20 14:29:59 +0500

    Arbeit mit Thema "13_FilesAndNetwork/homework_13.2"

--- 13_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,35 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
         // TODO: write code copy content of sourceDirectory to destinationDirectory
+
+        File srcDir = new File(sourceDirectory);
+        File destDir = new File(destinationDirectory);
+        try {
+            org.apache.commons.io.FileUtils.copyDirectory(srcDir, destDir);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        //zweite variante
+        File folder = new File(sourceDirectory);
+        File[] listOfFiles = folder.listFiles();
+
+        Path newDir = Paths.get(destinationDirectory);
+        if (listOfFiles != null)
+            for (File file : listOfFiles)
+                try {
+                    Files.copy(file.toPath(), newDir.resolve(file.getName())
+                            , StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
     }
 }
