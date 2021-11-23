d635e1e08cd02483f921d4f1e95ab338fb284d88
yuri018 <plktmn@gmail.com>
2021-11-23 10:13:20 +0500

    die Datei geändert pom.xml für Thema "13_FilesAndNetwork/homework_13.3"

--- 13_FilesAndNetwork/homework_11.4/src/main/java/ParsingSite.java
+++ 13_FilesAndNetwork/homework_11.4/src/main/java/ParsingSite.java
@@ -1,13 +1,23 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class ParsingSite {
 
     ParsingSite(String path) {
         try {
             Document doc = Jsoup.connect(path).get();
-            String title = doc.text();
+            String title = doc.title();
             System.out.println(title);
+
+            Elements newsHeadlines = doc.select("img");
+            for (Element headline : newsHeadlines) {
+                System.out.printf("%s\n\t%s\n%s",
+                        headline.attr("src"), headline.absUrl("href")
+                        , headline.attr("h3.card-title"));
+            }
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
