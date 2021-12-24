0dc32145348e596a402251fa3f8f8b48256900f5
Yuri018 <plktmn@gmail.com>
2021-12-24 13:18:43 +0500

    Arbeiten mit Modul "13_FilesAndNetwork/homework_13.5"

--- 13_FilesAndNetwork/homework_11.5/src/main/java/ParsingSite.java
+++ 13_FilesAndNetwork/homework_11.5/src/main/java/ParsingSite.java
@@ -1,5 +1,6 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -9,8 +10,17 @@ public class ParsingSite {
     ParsingSite(String path) {
         try {
             Document doc = Jsoup.connect(path).maxBodySize(0).get();
-            Elements selectLinks = doc.select("metrodata");
+//            Elements selectLines = doc.select("span.js-metro-line");
+//            for (Element link: selectLines){
+//                String linesNum = link.attr("data-line");
+//                System.out.println(linesNum + ". " + link.text());
+//            }
 
+            Elements selectStation = doc.select("span.name");
+            for (Element link: selectStation){
+                String stationNum = link.attr("span.name");
+                System.out.println(link.text());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
