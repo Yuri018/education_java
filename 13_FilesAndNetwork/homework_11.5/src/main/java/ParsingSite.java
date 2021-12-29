d0c14c1a1b01bb43fad7cb7f41489e4524b425ba
Yuri018 <plktmn@gmail.com>
2021-12-29 14:25:20 +0500

    json zu Maven hinzugefügt und Parsing-Methoden im Modul erstellt "13_FilesAndNetwork/homework_13.5"

--- 13_FilesAndNetwork/homework_11.5/src/main/java/ParsingSite.java
+++ 13_FilesAndNetwork/homework_11.5/src/main/java/ParsingSite.java
@@ -8,21 +8,31 @@ import java.io.IOException;
 public class ParsingSite {
 
     ParsingSite(String path) {
-        try {
-            Document doc = Jsoup.connect(path).maxBodySize(0).get();
-//            Elements selectLines = doc.select("span.js-metro-line");
-//            for (Element link: selectLines){
-//                String linesNum = link.attr("data-line");
-//                System.out.println(linesNum + ". " + link.text());
-//            }
 
-            Elements selectStation = doc.select("span.name");
-            for (Element link: selectStation){
-                String stationNum = link.attr("span.name");
-                System.out.println(link.text());
+        try {
+            parseLines(path);
+            parseStations(path);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+    //парсим номер и назавание линии
+    public static void parseLines (String path) throws IOException {
+        Document doc = Jsoup.connect(path).maxBodySize(0).get();
+            Elements selectLines = doc.select("span.js-metro-line");
+            for (Element link: selectLines){
+                String linesNum = link.attr("data-line");
+                System.out.println(linesNum + ". " + link.text());
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+    //парсим номер линии и название станций этой линии
+    public static void parseStations (String path) throws IOException {
+        Document doc = Jsoup.connect(path).maxBodySize(0).get();
+        Elements selectStation = doc.select("div.js-metro-stations");
+        for (Element link: selectStation){
+            String linesNum = link.attr("data-line");
+            Elements stationName = link.select("span.name");
+            System.out.println(linesNum + ". " + stationName.text());
         }
     }
 }
