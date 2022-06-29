51cce728727f3f6695d2613d8cf38293acbad2dd
Yuri018 <plktmn@gmail.com>
2022-06-29 11:12:58 +0200

    16_SQLAndHibernate Modul hinzugefügt

---
+++ 16_SQLAndHibernate/JDBCExperiments/jdbc-exp/src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/skillbox";
+        String user = "root";
+        String password = "testMysql";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Courses");
+            while (resultSet.next()) {
+                String courseName = resultSet.getString("name");
+                System.out.println(courseName);
+
+//                resultSet.close();
+//                statement.close();
+//                connection.close();
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
