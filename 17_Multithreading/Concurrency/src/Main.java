a2ff8f5e724cce6259f1b9d9edb17bc624f73b00
Yuri018 <plktmn@gmail.com>
2022-09-20 11:28:44 +0200

    17_Multitreading/ Prüfung von "Atomaren Variablen"

--- 17_Multithreading/Concurrency/src/Main.java
+++ 17_Multithreading/Concurrency/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
             new Thread(() -> {
-                for (int j = 0; j < 1000; j++) {
+                for (int j = 0; j < 100000; j++) {
                     ValueStorage.incrementValue();
                 }
                 System.out.println(ValueStorage.getValue());
--- 17_Multithreading/Concurrency/src/ValueStorage.java
+++ 17_Multithreading/Concurrency/src/ValueStorage.java
@@ -1,11 +1,13 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ValueStorage {
-    private static int value;
+    private static AtomicInteger value = new AtomicInteger();
 
     public static void incrementValue(){
-        value = value + 1;
+        value.incrementAndGet();
     }
 
     public static int getValue() {
-        return value;
+        return value.intValue();
     }
 }
