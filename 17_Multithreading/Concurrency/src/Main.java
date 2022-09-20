5951f80a128cd5318942c49e02c546cf03d527e4
Yuri018 <plktmn@gmail.com>
2022-09-20 11:22:48 +0200

    17_Multitreading/ Prüfung "Rennbedingungen und kritische Abschnitte"

---
+++ 17_Multithreading/Concurrency/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    ValueStorage.incrementValue();
+                }
+                System.out.println(ValueStorage.getValue());
+            }).start();
+        }
+    }
+}
---
+++ 17_Multithreading/Concurrency/src/ValueStorage.java
@@ -0,0 +1,11 @@
+public class ValueStorage {
+    private static int value;
+
+    public static void incrementValue(){
+        value = value + 1;
+    }
+
+    public static int getValue() {
+        return value;
+    }
+}
